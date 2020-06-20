9524e0f0273b4e65fb363fee0c252d62f67e9bdf
Dhananjay Shettigar <dhananjay.shettigar2252000@gmail.com>
2020-06-20 14:52:37 +0530

    prop details progress

--- js/property.js
+++ js/property.js
@@ -8,6 +8,10 @@ const description = document.getElementById("description_target");
 const amenities = document.getElementById("amenities_target");
 const doc1 = document.getElementById("doc1");
 const doc2 = document.getElementById("doc2");
+const plan_num = document.getElementById("plan_num");
+const plan_img = document.getElementById("plan_img");
+const similar_target = document.getElementById("similar_target");
+let data;
 
 const params = new URLSearchParams(window.location.search);
 
@@ -20,11 +24,15 @@ async function getPropertyData() {
             id: params.get("id")
         }
     });
-    const data = res.data[0];
-    price.innerHTML = "Rs. "+data.price;
-    address.innerHTML = data.address;
+    data = res.data[0];
+    let price_frame = "Rs. " + data.price;
+    if(data.duration && data.duration != "") {
+        price_frame = "Rs. " + data.price + " per " + data.duration;
+    }
+    price.innerHTML = price_frame;
+    address.innerHTML = '<i class="fas fa-map-marker-alt mr-2"></i>' + data.address;
     title.innerHTML = data.title;
-    contact.innerHTML = data.contact;
+    contact.innerHTML = '<i class="fas fa-phone-alt mr-2"></i>' +data.contact;
     key.innerHTML = data.details;
     description.innerHTML = data.description;
     image_target.innerHTML = "";
@@ -59,17 +67,100 @@ async function getPropertyData() {
             </div>
         `;
     }
-    img_frame.innerHTML += "</div>"
+    img_frame += "</div>"
     image_target.innerHTML += img_frame;
     res = await axios.get(`${url}property`, {
         params: {
             tags: data.tags
         }
     });
-	showSlides(slideIndex);
+    showSlides(slideIndex);
+    let num_amenities = data.features.length;
+    if(num_amenities%3 == 1) {
+        num_amenities = Math.floor(num_amenities/3)
+    } else {
+        num_amenities = Math.ceil(num_amenities/3);
+    }
+    count = 0;
+    amenities.innerHTML = "";
+    let amenities_frame = ""
+    for (feature of data.features) {
+        if(count % num_amenities == 0) {
+            amenities_frame += "<div class='col-md-4'>";
+        }
+        count++;
+        amenities_frame += `
+            <p>
+                <i class="fas fa-check-circle mr-3"></i>
+                ${feature}
+            </p>
+        `
+        if(count % num_amenities == 0) {
+            amenities_frame += "</div>"
+        }
+    }
+    amenities.innerHTML = amenities_frame;
+    if(data.documents[0]) {
+        doc1.href = `data:${data.documents[0].contentType};base64,${data.documents[0].file}`;
+        doc1.download = "New Document 1";
+    }
+    if(data.documents[1]) {
+        doc2.href = `data:${data.documents[1].contentType};base64,${data.documents[1].file}`;
+        doc2.download = "New Document 2";
+    }
+    count = 0;
+    plan_num.innerHTML = "Plans: "
+    if(data.plans && data.plans.length > 0) {
+        for (image of data.plans) {
+            count++;
+            plan_num.innerHTML += `<button class="but3" onclick="planImg(${count - 1})">${count}</button>&nbsp;`
+        }
+        plan_img.src = `data:${data.plans[0].contentType};base64,${data.plans[0].file}`;
+    } else {
+        plan_num.innerHTML += "None";
+    }
+
     let similar = res.data;
     similar = similar.filter(prop => prop._id != data._id);
+    similar_target.innerHTML = "";
+    for (prop of similar) {
+        let img = "";
+        if(prop.images && prop.images.length > 0) {
+            img = `data:${prop.images[0].contentType};base64,${prop.images[0].file}`
+        }
+        let sim_price = "Rs. " + prop.price;
+        if(prop.duration && prop.duration != "") {
+            sim_price = "Rs. " + prop.price + " per " + prop.duration;
+        }
+        similar_target.innerHTML += `
+            <div class="col-lg-3">
+                <div class="card" style="width: 15rem;">
+                    <img
+                        class="card-img-top"
+                        src="${img}"
+                        alt="Card image cap"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">${sim_price}</h5>
+                        <h5 id="card-text">
+                            ${prop.title}
+                        </h5>
+                        <p><i class="fas fa-bed mr-2"></i>Bedrooms: ${prop.bedrooms}</p>
+                        <p>
+                            <i class="fas fa-restroom mr-2"></i>Bathrooms: ${prop.bathrooms}
+                        </p>
+                        <p><i class="fas fa-car mr-2"></i>Floors: ${prop.floors}</p>
+                    </div>
+                </div>
+            </div>        
+        `;
+    }
 
+}
+
+//Change Plan Image on Click
+function planImg(num) {
+    plan_img.src = `data:${data.plans[num].contentType};base64,${data.plans[num].file}`
 }
 
 // Next/previous controls
